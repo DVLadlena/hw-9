533885979bd51afc60be362980bf0e1c59b2fa6c
Vladislava Larionova <jizntlen130@gmail.com>
2024-09-14 17:33:40 +0300

    изменено

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 console.log('hi');
+
+function additionFunction(a, b) {
+  return a + b;
+}
+
+let num1 = 30;
+let num2 = 20;
+let sum = additionFunction(num1, num2);
+console.log('Сумма чисел:', sum);
